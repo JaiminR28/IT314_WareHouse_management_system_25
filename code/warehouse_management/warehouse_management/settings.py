a49d544e1e4c4e240813985eb067bb644479b70e
Mohammadtej <tej.mohammad53@gmail.com>
2023-03-10 17:43:31 +0530

    Add Urls for the base application

--- code/warehouse_management/warehouse_management/settings.py
+++ code/warehouse_management/warehouse_management/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'warehouse.apps.WarehouseConfig',
+    'farmer.apps.FarmerConfig',
 ]
 
 MIDDLEWARE = [
--- code/warehouse_management/warehouse_management/urls.py
+++ code/warehouse_management/warehouse_management/urls.py
@@ -15,7 +15,16 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.urls import include
+from django.views.generic import RedirectView
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-]
+    path('farmer/', include('farmer.urls')),
+    path('warehouse/', include('warehouse.urls')),
+    path('', RedirectView.as_view(url='farmer/', permanent=True)),
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+

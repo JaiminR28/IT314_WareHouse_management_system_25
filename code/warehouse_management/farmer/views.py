9689e82810607833b631a0637b9d8936db00119b
arthdetroja01 <202001274@daiict.ac.in>
2023-03-12 21:52:27 +0530

    Warehouse Update Final

--- code/warehouse_management/farmer/views.py
+++ code/warehouse_management/farmer/views.py
@@ -3,9 +3,11 @@ from farmer.models import Farmer
 
 # Create your views here.
 
+def index(request):
+    return render(request, 'home.html')
+
 def login(request):
     return render(request, 'login.html')
-
 
 def loginValidate(request):
     if request.method == 'POST':

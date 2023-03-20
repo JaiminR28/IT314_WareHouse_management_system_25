59133afc82c1694a6646125ce4cc1669508a0a5b
arthdetroja01 <202001274@daiict.ac.in>
2023-03-20 15:53:26 +0530

    Warehouse

--- code/warehouse_management/warehouse/admin.py
+++ code/warehouse_management/warehouse/admin.py
@@ -1,9 +1,9 @@
-from django.contrib import admin
-from .models import Manager, Warehouse, Reservation, Crop_stored
+# from django.contrib import admin
+# from .models import Manager, Warehouse, Reservation, Crop_stored
 
-# Register your models here.
+# # Register your models here.
 
-admin.site.register(Manager)
-admin.site.register(Warehouse)
-admin.site.register(Reservation)
-admin.site.register(Crop_stored)
+# admin.site.register(Manager)
+# admin.site.register(Warehouse)
+# admin.site.register(Reservation)
+# admin.site.register(Crop_stored)
--- code/warehouse_management/warehouse/views.py
+++ code/warehouse_management/warehouse/views.py
@@ -1,14 +1,27 @@
 from django.shortcuts import render
-from warehouse.models import Manager, Warehouse
 from django.contrib import messages
+import pymongo
+from pymongo import MongoClient
+import re
 
 # Create your views here.
+# regex = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,7}\b'
+# def check(email):
+#     if(re.fullmatch(regex, email)):
+#         print("Valid Email") 
+#     else:
+#         print("Invalid Email")
+
+client = MongoClient('mongodb+srv://arth01:passadmin@cluster0.z4s5bj0.mongodb.net/?retryWrites=true&w=majority')
+db = client['demo']
+warehouse = db['Warehouse']
+manager = db['Manager']
 
 def index(request):
     return render(request, 'home.html')
 
 def login(request):
-    return render(request, 'login.html')
+    return render(request, 'w-login.html')
 
 
 def loginValidate(request):
@@ -16,22 +29,26 @@ def loginValidate(request):
         if request.POST.get('email') and request.POST.get('password'):
             email = request.POST.get('email')
             password = request.POST.get('password')
+            
+            query = {'email': email, 'password': password}
+            projection = {'email': 1}
 
-            user = Warehouse.objects.filter(email__exact=email, password__exact=password).values_list('first_name', flat = True)
-            if user:
+            users = manager.find(query, projection)
+            if len(list(users.clone())) == 1:
                 context = {
-                    'user' : user[0]
+                    'user' : users[0]['email']
                 }
-                return render(request, 'home.html', context=context)
+                return render(request, 'w-home.html', context=context)
             else:
                 messages.error(request, "Email or Password incorrect")
+                return render(request, 'w-login.html')
         else:
             messages.error(request, "Please enter credentails")
-            return render(request, 'login.html')
+            return render(request, 'w-login.html')
 
 
 def register(request):
-    return render(request, 'register.html')
+    return render(request, 'w-register.html')
 
 def registerEntry(request):
     if request.method == 'POST':
@@ -46,27 +63,38 @@ def registerEntry(request):
             latitude = request.POST.get('latitude')
             longitude = request.POST.get('longitude')
             storage_capacity = request.POST.get('storage_capacity')
-            print("*******************"+phoneNum+"***********************")
-            record1 = Warehouse(
-                name = name,
-                latitude = latitude,
-                longitude = longitude,
-                storage_capacity = storage_capacity,                
-            )
-            record1.save()
-            search = Warehouse.objects.get(latitude = latitude, longitude = longitude)
-            
-            record = Manager(
-                first_name = firstName, 
-                last_name = lastName,
-                phone_number = phoneNum,
-                email = email,
-                password = password,
-                warehouse_id = search
-            )           
-            record.save()
 
-            return render(request, 'login.html')
+            # if(not check(email)):
+            #     messages.error(request, "Invalid E-mail address")
+            #     return render(request, 'w-register.html')
+            
+            query = {'email': email}
+            projection = {'_id': 1}
+
+            users = warehouse.find(query, projection)
+
+            if len(list(users.clone())) != 0:
+                messages.error(request, 'Email already registered!')
+                return render(request, 'w-register.html')
+            
+            else:
+                warehouse.insert_one({
+                    'name': name,
+                    'latitude': latitude,
+                    'longitude': longitude,
+                    'storage_capacity': storage_capacity, 
+                    'email': email,
+                })
+                manager.insert_one({
+                    'first_name': firstName, 
+                    'last_name': lastName,
+                    'phone_number': phoneNum,
+                    'email': email,
+                    'password': password,
+                    'latitude': latitude,
+                    'longitude': longitude,
+                })
+                messages.success(request, 'Registration successful')
+                return render(request, 'w-login.html')
         else:
-            messages.error(request, "Enter details in all the fields")
-            return render(request, 'register.html')
+            return render(request, 'w-register.html')

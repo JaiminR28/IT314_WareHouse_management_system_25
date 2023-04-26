e8f5f5151a4d4448339919cf849f0f6e53cc3cbe
JaiminR28 <jaiminrathwa28@gmail.com>
2023-04-26 17:13:01 +0530

    Nearby warehouse added

--- code/warehouse_management/warehouse/views.py
+++ code/warehouse_management/warehouse/views.py
@@ -19,7 +19,7 @@ from reportlab.lib import colors
 from reportlab.lib.units import inch
 from reportlab.platypus import SimpleDocTemplate, Table, TableStyle, Paragraph, Spacer, Image
 from reportlab.lib.styles import getSampleStyleSheet, ParagraphStyle
-from io import BytesIO
+from io import BytesIO  
 
 
 # Create your views here.
